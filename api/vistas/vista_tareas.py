5d6fc6b4a0d71ba0a8726e7070f69431323c39a5
John Edwin Guerrero Ayala <je.guerreroa1@uniandes.edu.co>
2023-04-16 09:46:33 -0500

    feat: Adición de compresión al batch processor

--- api/vistas/vista_tareas.py
+++ api/vistas/vista_tareas.py
@@ -1,6 +1,4 @@
 # ZIP, 7Z, TAR.GZ, TAR.BZ2
-import zipfile
-import tarfile
 
 import os
 from flask import request
@@ -21,28 +19,6 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# def to_zip():
-#     print("Im a zip")
-#
-# def to_tar_bz2():
-#     print("Im a tar bz2")
-#
-# def to_tar_gz():
-#     print("im a tar gz")
-
-def to_zip(file_path, destination_path):
-    with zipfile.ZipFile(destination_path + '.zip', 'w') as zip_file:
-        zip_file.write(file_path)
-
-def to_tar_gz(file_path, destination_path):
-    with tarfile.open(destination_path + '.tar.gz', 'w:gz') as tar:
-        print(f'file url{file_path}')
-        tar.add(file_path, arcname=os.path.basename(file_path))
-
-def to_tar_bz2(file_path, destination_path):
-    with tarfile.open(destination_path + '.tar.bz2', 'w:bz2') as tar:
-        tar.add(file_path, arcname=os.path.basename(file_path))
-
 class VistaCreateTasks(Resource):
 
     def post(self):
@@ -50,11 +26,7 @@ class VistaCreateTasks(Resource):
         PROCESS_FOLDER = './processed'
         destination_format = request.form.get("to_format")
         print(f'to format -> {destination_format}')
-        formats = {
-            'zip': to_zip,
-            'tar_gz': to_tar_gz,
-            'tar_bz2': to_tar_bz2
-        }
+
 
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -79,17 +51,9 @@ class VistaCreateTasks(Resource):
 
         # Call to message broker for queue the file
         args = (filename, destination_format, datetime.utcnow())
-        proccess_file.apply_async(args=args, queue='files')
-
-        # TODO: Remove conversion step from here
-        if destination_format in formats.keys():
-            print(f"calling {destination_format}")
-            func = formats[destination_format]
-            print(f"function: {func}")
-            func(os.path.join(UPLOAD_FOLDER, filename), os.path.join(PROCESS_FOLDER, filenameParts[0]))
-            print(f"destination: { os.path.join(PROCESS_FOLDER, filename) }")
-        else:
-            print("Invalid format")
+        result = proccess_file.apply_async(args=args, queue='files')
+        task_id = result.id
+        print(f'New task id {task_id} {datetime.utcnow()}')
 
 
         return response_string, 200
--- jobs/app.py
+++ jobs/app.py
@@ -1,16 +1,44 @@
-# import time
-#
-# while True:
-#     print('Jobs component is running...')
-#     time.sleep(1)
-
+import zipfile
+import tarfile
 import os
 from celery import Celery
 
 celery_app = Celery(__name__, broker='redis://localhost:6379/0')
 
+def to_zip(file_path, destination_path):
+    with zipfile.ZipFile(destination_path + '.zip', 'w') as zip_file:
+        zip_file.write(file_path)
+
+def to_tar_gz(file_path, destination_path):
+    with tarfile.open(destination_path + '.tar.gz', 'w:gz') as tar:
+        print(f'file url{file_path}')
+        tar.add(file_path, arcname=os.path.basename(file_path))
+
+def to_tar_bz2(file_path, destination_path):
+    with tarfile.open(destination_path + '.tar.bz2', 'w:bz2') as tar:
+        tar.add(file_path, arcname=os.path.basename(file_path))
+
 @celery_app.task(name='proccess_file')
 def proccess_file(filename, new_format, fecha):
+    UPLOAD_FOLDER = './uploads'
+    PROCESS_FOLDER = './processed'
+    filenameParts = filename.split('.')
+
     log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'log_conversion.txt')
     with open(log_file_path, 'a+') as file:
         file.write('{} to {} - solicitud de conversion: {}\n'.format(filename, new_format, fecha))
+
+    formats = {
+        'zip': to_zip,
+        'tar_gz': to_tar_gz,
+        'tar_bz2': to_tar_bz2
+    }
+
+    if new_format in formats.keys():
+        print(f"calling {new_format}")
+        func = formats[new_format]
+        print(f"function: {func}")
+        func(os.path.join(UPLOAD_FOLDER, filename), os.path.join(PROCESS_FOLDER, filenameParts[0]))
+        print(f"destination: {os.path.join(PROCESS_FOLDER, filename)}")
+    else:
+        print("Invalid format")
