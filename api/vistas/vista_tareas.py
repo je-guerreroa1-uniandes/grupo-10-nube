262b207349863ba6e150712e86f3a69053a2bec9
John Edwin Guerrero Ayala <je.guerreroa1@uniandes.edu.co>
2023-04-16 17:04:50 -0500

    fix: Remove comments

--- api/vistas/vista_tareas.py
+++ api/vistas/vista_tareas.py
@@ -7,17 +7,7 @@ from werkzeug.utils import secure_filename
 from datetime import datetime
 from celery import Celery
 
-import configparser
-
-# config = configparser.ConfigParser()
-# # config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'config.ini'))
-# config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config.ini'))
-# print(config_path)
-# print(config.items())
-# # REDIS_DATABASE_URI = config.get('redis', 'REDIS_DATABASE_URI')
-
 celery_app = Celery(__name__, broker='redis://:lOGleSPirDOLEYsiceWlemPtO@10.130.13.4:6379/0')
-# celery_app = Celery(__name__, broker=app.config["REDIS_DATABASE_URI"])
 @celery_app.task(name='proccess_file')
 def proccess_file(*args):
     pass
