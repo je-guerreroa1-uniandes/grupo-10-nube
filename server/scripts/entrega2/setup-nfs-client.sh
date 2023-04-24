4c46a088982f98f9d715dc0ec975fa4a90c720cf
Juan Manuel González Garzón <jmgonzalezgarzon@gmail.com>
2023-04-23 21:54:15 -0500

    Correcciones a los scripts y docker-compose para asegurar que el usuario tiene permisos sobre la carpeta nfs

--- server/scripts/entrega2/setup-nfs-client.sh
+++ server/scripts/entrega2/setup-nfs-client.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 
 sudo apt update
-sudo apt install nfs-common
+sudo apt -y install nfs-common
 sudo mkdir -p /nfs/general
 sudo mount 10.120.0.3:/var/nfs/general /nfs/general
 
 # Verificar que se haya montado correctamente
-du -h
+sudo du -h
 
-grep '10.120.0.3' /etc/fstab 2>&1 >/dev/null && sudo sed -i '/10.120.0.3/d' /etc/fstab
-sudo echo '10.120.0.3:/var/nfs/general /nfs/general nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' >> /etc/fstab
+sudo grep '10.120.0.3' /etc/fstab 2>&1 >/dev/null && sudo sed -i '/10.120.0.3/d' /etc/fstab
+sudo echo '10.120.0.3:/var/nfs/general /nfs/general nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' | sudo tee -a /etc/fstab
