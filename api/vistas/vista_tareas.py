547bacb42d245dc1014d1fbcd0a9e29bb1a27e3e
John Edwin Guerrero Ayala <je.guerreroa1@uniandes.edu.co>
2023-05-21 02:02:49 -0500

    feat: Update saving location

--- api/vistas/vista_tareas.py
+++ api/vistas/vista_tareas.py
@@ -75,13 +75,10 @@ class VistaCreateTasks(Resource):
         blob_name = f"{current_user['id']}/{filenameParts[0]}_{datetime.utcnow().strftime('%Y%m%d%H%M%S')}.{extension}"
         blob = bucket.blob(blob_name)
 
-        # Upload the file to Google Cloud Storage
-        blob.upload_from_filename(file_path)
-
         # Assuming you have a cloud store object called "blob"
         folder_path = "general/uploads/"  # Specify the desired folder path
         destination_path = folder_path + filename
-        blob.upload_from_filename(file_path, destination_path)
+        blob.upload_from_filename(destination_path)#(file_path, destination_path)
 
         new_file = File(
             filename=secure_filename(file.filename),
