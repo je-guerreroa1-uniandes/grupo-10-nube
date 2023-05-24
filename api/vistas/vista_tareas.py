6458e2a93e2389a9429a9a33deda41319da3b1d3
John Edwin Guerrero Ayala <je.guerreroa1@uniandes.edu.co>
2023-05-23 21:44:48 -0500

    feat: add more temp file retries

--- api/vistas/vista_tareas.py
+++ api/vistas/vista_tareas.py
@@ -132,12 +132,12 @@ class VistaCreateTasks(Resource):
         print(f'Published message with ID: {message_id}')
 
         attempt_counter = 0
-        max_attempts = 20
+        max_attempts = 30
         while attempt_counter < max_attempts and not os.path.exists(file_path):
             attempt_counter += 1
             print(f"Attempt {attempt_counter}: File not found. Waiting 0.5 seconds...")
             time.sleep(0.5)
-            
+
         os.remove(file_path)
 
         return response_string, 200
